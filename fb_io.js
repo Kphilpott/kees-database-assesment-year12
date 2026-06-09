1326a3818d17ddcc7ec464970298f4ded244c0f7
Kphilpott <23424kp@hvhs.school.nz>
2026-06-09 14:59:46 +1200

    0.2.2

--- fb_io.js
+++ fb_io.js
@@ -28,7 +28,7 @@ console.log("Hello World");
 		DodgetheBallsScore: score,
       }
     )
-    document.getElementById("DodgeScore").innerHTML = "Your last Score was: " + score;
+    document.getElementById("DodgeScore").innerHTML = "Version 1.6.3.  Your last Score was: " + score;
 }
 
 function writeFormGDash() {
